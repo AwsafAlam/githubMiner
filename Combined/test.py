dcdda27609fc1bde4feccf83c70e3b38ad992742
abhik1505040 <1505040ab@ugrad.cse.buet.ac.bd>
2019-05-17 22:03:35 +0600

    bug fix

--- Combined/test.py
+++ Combined/test.py
@@ -14,17 +14,16 @@ def parse(repo_full_name):
     contents = repo.get_contents("")
     while len(contents) >= 1:
         file_content = contents.pop(0)
-        path=""
+        #was previously assigning it here for printing...path=file_content.path 
         if file_content.type == "dir":
             if not os.path.exists(parent_dir + file_content.path.replace("/", "\\")):
                 os.makedirs(parent_dir + file_content.path.replace("/", "\\"))
             contents.extend(repo.get_contents(file_content.path))
         else:
             path = file_content.path
-        
-        filename, extension = os.path.splitext(path)
-        if extension in config.required_file_types:
-            urllib.request.urlretrieve(base_url+repo_url+path, parent_dir+path.replace("/", "\\") )
+            filename, extension = os.path.splitext(path)
+            if extension in config.required_file_types:
+                urllib.request.urlretrieve(base_url+repo_url+path, parent_dir+path.replace("/", "\\") )
     
     return 1  #downloaded required files
 
